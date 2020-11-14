49e74fac87168da88de1ae53be35dc75bb96d20c
appregion <58521393+appregion@users.noreply.github.com>
2020-11-15 00:40:03 +0100

    Delete api_keys.dart

--- lib/app/resources/api_keys.dart
+++
@@ -1,7 +0,0 @@
-class ApiKeys {
-  String openWeatherApiKey = 'ce3cfe3cb651ea67676b5d84df035fc4';
-  String googlePlacesApiKey = 'AIzaSyBSxH7o6uHoC_Ja1U6HTKwEYFi4ehhgWUg';
-  String aviaSalesApiKey = '20a5e8b3156edfbc3f2aab405d13d0a5';
-  String flickrApiKey = '260c8fe4eeb8fd6a04a6c8c80e175d25';
-  String googleImageApiKey = 'AIzaSyAYvxvPsW0RT8XHy8nTUOj2C6KIvlR0g68';
-}
